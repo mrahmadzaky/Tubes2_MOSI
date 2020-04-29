7260ae7a0c10c00495a9dd915414e65d62581fb2
Ahmad Zaky <37398116+mrahmadzaky@users.noreply.github.com>
2020-04-29 20:24:04 +0700

    Updating
    Penambahan Comment

--- TUBES2.py
+++ TUBES2.py
@@ -37,7 +37,7 @@ for i in range(jum_indiv):
     if kesehatan[i] == "terinfeksi":
         posxInfected.append(posx[i])
         posyInfected.append(posy[i])
-
+### Membuat Fungsi PBC ###
 def PBCx(x):
     if x > x_max:
         x += -x_max
@@ -54,11 +54,11 @@ def PBCy(y):
 
 totalinfected = virus
 totalsembuh = 0
-while totalsembuh != totalinfected:
+while totalsembuh != totalinfected: #Akan loop sampai semua yang terinfeksi sembuh
     for i in range(jum_indiv):
         x = [0,1]
         weights = [probability,.2]
-        b = random.choices(x,weights)
+        b = random.choices(x,weights) #Random Probability
         if b[0] == 0: #Jika individu bergerak
             nilai = random.uniform(0,1)
             if nilai <= 0.25:
@@ -73,10 +73,10 @@ while totalsembuh != totalinfected:
             else:
                 posy[i]=posy[i]+1
                 posy[i]=PBCy(posx[y])
-        if kesehatan[i] == "terinfeksi":
+        if kesehatan[i] == "terinfeksi": ### Jika individu terinfeksi, maka di cek infected time nya sudah berapa lama#
             infectedtime[i] = infectedtime[i]+1
             total_pemulihan_komunitas = total_pemulihan_komunitas + 1
-            if infectedtime[i] >= waktu_pemulihan:
+            if infectedtime[i] >= waktu_pemulihan: ### Jika sudah 10 Hari, maka dinyatakan pulih dan imun menjadi kebal
                 kesehatan[i] = "pulih"
                 imunitas[i] = "kuat"
                 if not posxInfected:
@@ -85,10 +85,10 @@ while totalsembuh != totalinfected:
                     posxInfected.pop(0)
                     posyInfected.pop(0)
         for j in range(len(posxInfected)):
-            if posx[i]==posxInfected[j] and posy[i]==posyInfected[j]:
-                if totalinfected < jum_indiv and kesehatan[i] == "belum_terinfeksi":
+            if posx[i]==posxInfected[j] and posy[i]==posyInfected[j]: ### Melakukan perbandingan jarak terinfeksi dan non terinfeksi
+                if totalinfected < jum_indiv and kesehatan[i] == "belum_terinfeksi": # Jika individu masih blm terinfeksi, maka dia tertular
                     kesehatan[i] = "terinfeksi"
-                    totalinfected = totalinfected + 1
+                    totalinfected = totalinfected + 1 ##Bertambahnya individu yang tertular corona
                     posxInfected.append(posx[i])
                     posyInfected.append(posy[i])
     totalsembuh = totalsembuh + 1
